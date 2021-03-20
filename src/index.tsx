51f1d219408eb9293ffee029e234beb140dc01d8
Tux <tuxsnct@gmail.com>
2021-03-20 06:56:13 +0000

    feat: update website design

--- src/index.tsx
+++ src/index.tsx
@@ -17,17 +17,17 @@ const global = css`
     padding: 0;
   }
 `
-const Main = tw.main`p-6 box-border bg-gray-100 dark:bg-gray-900`
+const Main = tw.main`px-6 pt-6 pb-32 box-border bg-gray-100 dark:bg-gray-900`
 const Heading = tw.h1`mx-4 mt-4 mb-8 text-5xl text-gray-900 dark:text-gray-100 break-words`
 const LinkCollection = tw.nav`m-4`
 const LinkItem = tw.a`
   block w-full mb-4 px-4 py-2 bg-gray-900 dark:bg-gray-100 rounded no-underline box-border
   text-xl text-gray-100 dark:text-gray-900 whitespace-nowrap overflow-hidden overflow-ellipsis
 `
-const Footer = tw.footer`p-4 bg-yellow-500`
-const FooterSocial = tw.div`flex items-center justify-center h-12 m-2`
-const FooterCopyright = tw.span`block m-2 text-lg text-gray-900 text-center`
-const IconLinkContainer = tw.a`flex items-center justify-center w-12 mx-2 h-full text-gray-900`
+const Footer = tw.footer`fixed inset-x-0 bottom-0 p-2 bg-yellow-500`
+const FooterSocial = tw.div`flex items-center justify-center h-10 m-2`
+const FooterCopyright = tw.span`block text-lg text-gray-900 text-center`
+const IconLinkContainer = tw.a`flex items-center justify-center w-10 mx-2 h-full text-gray-900`
 const IconLink = ({ href, icon, ...rest }) => {
   return (
     <IconLinkContainer href={href}>
@@ -48,7 +48,7 @@ const Welcome: FunctionalComponent = () => {
         <LinkCollection>
           <LinkItem href="https://blog.tuxsnct.com">Blog</LinkItem>
           <LinkItem href="https://docs.tuxsnct.com">Documents</LinkItem>
-          <LinkItem href="https://prj.tuxsnct.com">Projects</LinkItem>
+          <LinkItem href="https://projects.tuxsnct.com">Projects</LinkItem>
           <LinkItem href="https://about.tuxsnct.com">Portfolio</LinkItem>
         </LinkCollection>
       </Main>
